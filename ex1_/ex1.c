038b868fc8ea103e37a1693899ad2af9f14769dc
perfectswpuboy1 <jimmy2752@gmail.com>
2019-03-28 02:06:14 +0800

    start

---
+++ ex1_/ex1.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+void main()
+{
+	int a=5;
+	char c='a';
+	float f=5.3;
+	double m=12.65;
+	double result;
+	
+	printf ("a+c = %d \n",a+c);
+	result = a +c *(f+m);
+	printf("result is %f\n",result);
+
+
+
+}
